8e300b7f6b9fb11435a4b0e8c84e47d33f25fb99
mz7701 <korean2071@naver.com>
2022-05-06 23:12:38 +0900

    ee

--- my-app-blog/server/server.js
+++ my-app-blog/server/server.js
@@ -1,11 +1,60 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const bodyParser = require('body-parser')
+const sequelize = require('./models').sequelize;
+sequelize.sync();
 
-app.get('/', (req, res) => {
-    res.send(`Response Complate`);
+//sequelize.sync({ force: true });
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const {
+    Teacher,
+    Sequelize: { Op }
+  } = require('./models');
+sequelize.query('SET NAMES utf8;');
+
+app.post('/add/data', (req, res) => {
+    console.log(req.body)
+
+      Teacher.create({
+          name : req.body.data
+      })
+      .then( result => {
+          res.send(result)
+      })
+      .catch( err => {
+          console.log(err)
+          throw err;
+      })
+}) 
+//
+app.post('/add/data', (req, res) => {
+    console.log(req.body)
+})//
+
+app.get('/get/data', (req, res) => {
+    Teacher.findAll()
+    .then( result => { res.send(result) })
+    .catch( err => { throw err })
+}) 
+app.post('/modify/data', (req, res) => {
+    Teacher.update({ name : req.body.modify.name }, {
+        where : { id : req.body.modify.id }
+    })
+    .then( result => { res.send(result) })
+    .catch( err => { throw err })
 })
 
+app.post('/delete/data', (req, res) => {
+    Teacher.destroy({
+        where : { id : req.body.delete.id }
+    })
+    .then( res.sendStatus(200) )
+    .catch( err => { throw err })
+})
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server On : http://localhost:${PORT}/`);
+    console.log(`Server On : http://localhost:${PORT}/`);
 })
